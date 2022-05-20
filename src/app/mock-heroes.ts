7ea89fe1d2329320c10706f69d8c8e44b8def959
TessaPJ99 <paul.mary11299@gmail.com>
2022-05-20 10:19:33 +0530

    first few commits

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES : Hero []=[
+    {id:2,name:"Wilson"},
+    {id:3,name:"Mathew"},
+    {id:4,name:"John"},
+    {id:5,name:"Fred"},
+    {id:6,name:"Harry"},
+    {id:7,name:"Diana"},
+    {id:8,name:"Charles"},
+    {id:9,name:"Jacob"},
+
+]
