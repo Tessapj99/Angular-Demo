8515471af23447ebb913592eb28e06be594dec93
TessaPJ99 <paul.mary11299@gmail.com>
2022-05-20 14:14:16 +0530

    Services Injected

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
    
-  heroes = HEROES;
+  heroes:Hero [] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+
+  }
+
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
 
 selectedHero?: Hero;
 onSelect(hero: Hero): void {
